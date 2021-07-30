1c97b1cd233083c9544664ff7b21423e36af92a5
nayeon <vennie20@naver.com>
2021-07-30 14:44:06 +0900

    0730

---
+++ 1927.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	int n, x;
+	priority_queue<int, vector<int>, greater<int> > q;
+
+	cin >> n;
+	while (n--)
+	{
+		cin >> x;
+		if (x == 0 && q.empty())
+			cout << 0 << '\n';
+		else if (x == 0)
+		{
+			cout << q.top() << '\n';
+			q.pop();
+		}
+		else
+			q.push(x);
+	}
+	return 0;
+}
+
