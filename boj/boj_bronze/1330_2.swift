2a437c40cb4a89cac3fd0924ed2661714c19b10f
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2022-05-13 11:22:34 +0900

    220513 조건문 포스팅

--- boj/boj_bronze/1330_2.swift
+++ boj/boj_bronze/1330_2.swift
@@ -1,9 +1,10 @@
 import Foundation
 
-let a = Int(readLine()!)!
-switch a {
-    case let x where x < 0 : print("negative")
-    case let x where x == 0 : print("zero")
-    default : print("positive")
+let nums = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+switch nums[0] - nums[1] {
+    case let x where x < 0 : print("<")
+    case let x where x == 0 : print("==")
+    default : print(">")
 }
 
