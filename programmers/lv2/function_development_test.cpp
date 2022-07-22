6d461ebf95cc3c344816ade0540c872cada311a9
nayeon <vennie20@naver.com>
2022-07-22 16:39:57 +0900

    220722

--- programmers/lv2/function_development_test.cpp
+++ programmers/lv2/function_development_test.cpp
@@ -4,18 +4,22 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> day;
-    for (int i = 0; i < progresses.size(); i++) {
+    
+    for (int i = 0; i < progresses.size(); i++){
         int tmp = (100 - progresses[i]) / speeds[i] + ((100 - progresses[i]) % speeds[i] != 0);
         day.push_back(tmp);
     }
-    int cnt = 1;
-    for (int i = 0; i < day.size(); i++) {
-        if (i == day.size() - 1 || day[i] < day[i + 1]) {
+    int cnt = 0;
+    int saveidx = 0;
+    for (int i = 0; i < day.size(); i++){
+        if (day[saveidx] < day[i]){
             answer.push_back(cnt);
             cnt = 1;
-        }
-        else
+            saveidx = i;
+        } else {
             cnt++;
+        }
     }
+    answer.push_back(cnt);
     return answer;
 }
